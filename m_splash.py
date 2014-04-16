e8d82042bf00636006af62bc09fe1f1da9db4b3d
Artem Abramov <aabramovrussia@yandex.com>
2014-04-16 15:06:34 +0400

    splash with melt
    The splash screen now has a melting effect instead of fading.

--- m_splash.py
+++ m_splash.py
@@ -8,8 +8,100 @@ import pygame
 import util
 from elements import ElementBase
 import resources
+import random
 
 
+def myrandom(n):
+    # another layer of overhead...
+    return random.randrange(n)
+
+class Meltdown:
+    def __init__(self, surface, min_size=90, max_size=100, min_dist=50,
+            min_width=30, width_add=20, finished=50):
+        """
+        min_size=10,
+        max_size=100,
+        min_dist=10,
+        min_width=30,
+        width_add=20,
+        finished=50
+        """
+
+
+        self.surf = surface
+
+        self.WIDTH, self.HEIGHT = surface.get_size()
+
+        self.MIN_SIZE = min_size
+        self.MAX_SIZE = max_size
+
+        self.MIN_DIST = min_dist
+
+        self.MIN_WIDTH = min_width
+        self.WIDTH_ADD = width_add
+
+        self.MAX_HEIGHT = self.HEIGHT - self.MIN_SIZE
+        self.FINISHED = finished
+
+        self.heights = []
+        while len(self.heights) < self.WIDTH: self.heights.append(0)
+
+        self.finished = 0
+
+
+    def step(self):
+        # redefinitions to speed things up a little bit
+        WIDTH = self.WIDTH
+        MIN_WIDTH = self.MIN_WIDTH
+        HEIGHT = self.HEIGHT
+        MAX_HEIGHT = self.MAX_HEIGHT
+
+        rand = myrandom
+        heights = self.heights
+        surf = self.surf
+
+        _range = range
+        _min = min
+        _max = max
+
+        # precalculations
+        width = rand(MIN_WIDTH) + self.WIDTH_ADD
+
+        #xloc = self.calc_xloc(width)
+        xloc = rand(WIDTH + MIN_WIDTH) - 2*MIN_WIDTH
+        xloc = _max(_min((xloc + width), (WIDTH - width)), 0)
+
+        yloc = HEIGHT
+        for i in _range(xloc, (xloc + width)):
+            yloc = _min(yloc, heights[i])
+        if yloc == HEIGHT:
+            return surf, []
+
+        # calculate block size
+        dist = rand(yloc/10 + self.MIN_DIST)
+        size = rand(_max(yloc + self.MIN_SIZE, self.MAX_SIZE))
+
+        # define rects for blitting
+        destpos = (xloc, yloc + dist)
+        destrect = (xloc, yloc, width, dist)
+        sourcerect = (xloc, yloc, width, size)
+
+        # the visible bit
+        surf.blit(surf, destpos, sourcerect)
+        surf.fill((0, 0, 0), destrect)
+
+        # postcalculations
+        yloc += dist
+        for i in _range(xloc, (xloc + width)):
+            if heights[i] < MAX_HEIGHT and yloc >= MAX_HEIGHT:
+                self.finished += 1
+            heights[i] = _max(heights[i], yloc)
+
+        if self.finished >= (WIDTH - self.FINISHED):
+            return None, []
+
+        # everything is fine, return the new surface
+        return surf, [sourcerect, destrect]
 
 
 class Splash( ElementBase ):
@@ -80,12 +172,29 @@ class SplashModule( util.ModuleBasic ):
         splash = pygame.image.load( self.image_path ).convert()
         splash = pygame.transform.smoothscale( splash, (800,600) )
 
-        for i in range(27):
-            pygame.event.pump()     # tell OS that we are processing events otherwise it thinks the app is frozen
-            splash.set_alpha(i)
-            screen.blit(splash, (0,0))
-            pygame.display.update()
-            pygame.time.wait(100)
+        # blit the surface to the screen
+        screen.blit(splash, (0,0))
+        pygame.display.flip()
+
+        # instantiate the Meltdown class
+        meltdown = Meltdown( splash )
+
+        # first step
+        surface, dirty = meltdown.step()
+
+        # main loop
+        while surface:
+            pygame.event.pump()     # tell OS that we are processing the events and are not simply frozen.
+
+            #surface, dirty = meltdown.step()
+
+            # redraw screen
+            screen.blit( surface, (0,0))
+            pygame.display.update( dirty )
+
+            # next step
+            surface, dirty = meltdown.step()
+
 
         pygame.mixer.fadeout(2000)
         screen.blit( splash,(0,0) )
